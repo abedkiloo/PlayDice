2f3cbc67b1993cf445835fa637c4d2077ec08117
abedkiloo <abedxh@gmail.com>
2020-02-04 08:26:26 +0300

    rolling dice with image of different random selection

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,18 +3,19 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private var textRollDice: TextView? = null
+    private var imageViewRollDice: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 /*text*/
-        textRollDice = findViewById(R.id.tv_roll_dice_text)
+        imageViewRollDice = findViewById(R.id.iV_roll_dice_text)
 /*button roll dice*/
         val btnRollDice: Button = findViewById(R.id.btn_roll_dice)
         btnRollDice.setOnClickListener {
@@ -24,7 +25,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val numberRolled = Random.nextInt(6) + 1
-        textRollDice?.text = numberRolled.toString()
 
+        val drawableResource = when (numberRolled) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        imageViewRollDice?.setImageResource(drawableResource)
     }
 }
